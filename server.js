370f595e343b95d30cffcadbbe62ea800a4b8de1
ArturBekhDEV <102412173+ArturBekhDEV@users.noreply.github.com>
2022-10-10 17:53:31 +0300

    added mysql db

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
-import express from "express";
+const express = require("express");
+
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "ArturData1!",
+  database: "form_contact",
+});
 
 const PORT = 5000;
 
-const app = express();
-
 app.get("/", (req, res) => {
-  res.status(200).json("Server working!!");
+  const sqlInsert =
+    "INSERT INTO contact_db (name, surname, email) VALUES ('artur', 'bekh', 'artbekh@gmail.com')";
+
+  db.query(sqlInsert, (err, result) => {
+    res.send("hello its test new");
+    console.log("error", err);
+    console.log("result", result);
+  });
 });
 
-app.listen(PORT, () => console.log("server started!!"));
+app.listen(PORT, () => console.log("server started!"));
